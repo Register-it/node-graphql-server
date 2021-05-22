0a00c2d165d39846f6a1c1848584cc41450b44da
Tiziano Pessa <tiziano.pessa@register.it>
2021-05-22 15:08:34 +0200

    resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,23 @@
 const { restaurants, reviews } = require('./datasource.js');
 
 module.exports = {
-
+    Query: {
+        restaurants: function(_parent, args) {
+            if (args.city === undefined) {
+                return restaurants;
+            } else {
+                return restaurants.filter(restaurant => restaurant.city === args.city);
+            }
+        },
+    },
+    Restaurant: {
+        reviews: function(restaurant, args) {
+            if (args.rating === undefined) {
+                return reviews.filter(review => review.restaurantId === restaurant.id);
+            } else {
+                return reviews.filter(review => review.restaurantId === restaurant.id)
+                    .filter(review => review.rating >= args.rating);
+            }
+        },
+    },
 };
